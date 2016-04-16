b1287a9adde5bad4fdab6b8f13a418e482dfc9ee
Aditya Joshi <adi3230@gmail.com>
2016-04-16 14:36:34 +0200

    adjust indentation

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
+   gutil = require('gulp-util');
 
 //Create a basic taks to check if Gulp runs properly
 gulp.task('check', function () {
-	return gutil.log('Gulp is up and running!!!');
+   return gutil.log('Gulp is up and running!!!');
 });
